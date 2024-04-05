506185030f7ef4c1224eb32652a45d5ea0a2bef9
rjcarne <45114248+rjcarne@users.noreply.github.com>
2024-04-05 18:00:45 -0400

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,11 @@
+<!-- public function invoke($functionName, $params, $logType = 'None')
+    {
+        return $this->lambdaClient->invoke([
+            'FunctionName' => $functionName,
+            'Payload' => json_encode($params),
+            'LogType' => $logType,
+        ]);
+    } -->
+
+
+echo "My first PHP script!";
